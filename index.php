f0d2566f87544d7450f0f05377eee072c27ae355
REBECA RODRIGUES DE PRIMO <rebeca.rprimo@senacsp.edu.br>
2022-02-17 22:12:03 -0300

    aula 2 - arrays e vetor

--- index.php
+++ index.php
@@ -1,9 +1,57 @@
 <?php
 
-echo "olá mundo! ola mundo mais legal<br><br>";
+//comentário de linha
 
-for($i = 0; $i < 10; $i++){
-    echo "um monte de olá mundo!<br>"
-}
+/*
+comentário de bloco
+*/
 
-echo "teste alteração de arquivo!"
+//define('XPTO', 'valor sempre igual'); 
+//define serve para definir constantes; a ordem eh o nome da constante + o valor que não se altera
+
+//echo XPTO;
+
+$num = 2;
+
+$var = '<br>' . "olá mundo! $num"; 
+//o ponto eh o operador de concatenação;
+//para interpolar dados eh necessário aspas duplas;
+//aspas simples são apenas para strings sozinhas. 
+
+//echo $var;
+
+//var_dump($var);
+
+//phpinfo(); informações importantes, como variavéis do ambiente
+
+/*$domingo = 'domingo';
+$segunda = 'segunda-feira';
+$terca = 'terça-feira';
+$quarta = 'quarta-feira';
+$quinta = 'quinta-feira';
+$sexta = 'sexta-feira';
+$sabado = 'sábado';*/
+
+$diaSemana = [1 => 'domingo', 
+              2 => 'segunda-feira', 
+              5 => 'terça-feira', 
+              8 => 'quarta-feira', 
+              10 => 'quinta-feira', 
+              3 => 'sexta-feira', 
+              4 => 'sábado'];
+
+echo "<pre>";
+
+//$contatos = ['carol', 'camilla', 'robson'];
+//var_dump($contatos);
+
+//echo "<br>hoje eh {$diaSemana[10]}"; //exemplo sem interpolação
+//echo '<br>hoje eh ' . $diaSemana[2]; //exemplo com interpolação
+
+$usuario[0] = ['nome' => 'Luiz', 'senha' => '123'];
+$usuario[1] = ['nome' => 'Fer', 'senha' => '321'];
+$usuario[2] = ['nome' => 'Bono', 'senha' => '627'];
+
+echo "<pre>\n\n";
+
+var_dump($usuario);
